b95e4f5628cc03e12b5dac5955d3920cf29a4bf8
Andres Espinosa <andreseespinosao@gmail.com>
2023-05-30 22:43:02 -0500

    prototype exercise

---
+++ 4. Prototype/3. prototype_exercise.py
@@ -0,0 +1,16 @@
+"""Prototype Coding Exercise
+Given the definitions shown in code, you are asked to implement Line.deep_copy()  to perform a deep copy of the given Line  object. This method should return a copy of a Line that contains copies of its start/end points.
+Note: please do not confuse deep_copy() with __deepcopy__()!"""
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, start=Point(), end=Point()):
+        self.start = start
+        self.end = end
+
+    def deep_copy(self):
+        # TODO
