b7a4a91e8333d2da0da154d53d17ae9baa874eac
Andres Espinosa <andreseespinosao@gmail.com>
2023-06-01 23:15:09 -0500

    proxy exercise

--- 12. Proxy/3. proxy_exercise.py
+++ 12. Proxy/3. proxy_exercise.py
@@ -22,5 +22,24 @@ class Person:
 class ResponsiblePerson:
   def __init__(self, person):
     self.person = person
+    
+  @property
+  def age(self):
+    return self.person.age
 
-  # todo: rest of this class
+  @age.setter
+  def age(self, value):
+    self.person.age = value
+  def drink(self):
+      if self.person.age >= 18:
+          return self.person.drink()
+      return 'too young'
+  
+  def drive(self):
+      if self.person.age >= 16:
+          return self.person.drive()
+      return 'too young'
+  
+  def drink_and_drive(self):
+    return 'dead'
+      
