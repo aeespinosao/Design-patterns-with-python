2d9fa08a9b81bc8e445c561ebd07f7453aa55a3a
Andres Espinosa <andreseespinosao@gmail.com>
2023-08-28 23:21:00 -0500

    singleton design pattern

---
+++ Python design patterns/naive_singleton.py
@@ -0,0 +1,36 @@
+from typing import Any
+from threading import Thread
+import time
+
+class Singleton(type):
+    _instances = {}
+    
+    def __call__(self, *args: Any, **kwargs: Any) -> Any:
+        if self not in self._instances:
+            instance = super().__call__(*args, **kwargs)
+            time.sleep(1)
+            self._instances[self] =  instance
+        return self._instances[self]
+    
+class NetworkDriver(metaclass=Singleton):
+    
+    def log(self):
+        print(f"{self} -> {id(self)}\n")
+        
+        
+def create_singleton():
+    singleton = NetworkDriver()
+    singleton.log()
+    return singleton
+
+if __name__ == "__main__":
+    # Single thread
+    s1 = create_singleton()
+    s2 = create_singleton()
+    
+    # Multi thread
+    t1 = Thread(target=create_singleton)
+    t2 = Thread(target=create_singleton)
+    
+    t1.start()
+    t2.start()
