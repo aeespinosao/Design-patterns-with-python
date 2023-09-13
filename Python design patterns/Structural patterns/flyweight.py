3c55f11f100128bce1ac3f6c2e3dea5ebef5789a
Andres Espinosa <andreseespinosao@gmail.com>
2023-09-12 23:09:07 -0500

    add flywight pattern

---
+++ Python design patterns/Structural patterns/flyweight.py
@@ -0,0 +1,68 @@
+import random
+from abc import ABC, abstractmethod
+
+class Sprite(ABC):
+    
+    @abstractmethod
+    def draw(self):
+        pass
+    
+    @abstractmethod
+    def move(self, x: int, y: int):
+        pass
+    
+
+class FighterRank:
+    private = 0
+    sergeant = 1
+    major = 2
+    
+class Fighter(Sprite):
+    
+    def __init__(self, rank: FighterRank) -> None:
+        self.rank = rank
+        
+    def draw(self):
+        print(f"Drawing fighter {self}")
+    
+    def move(self, x: int, y: int):
+        print(f"Moving fighter {self} to position {x}, {y}")
+        
+
+class FighterFactory:
+    
+    def __init__(self) -> None:
+        self.fighters = []
+    
+    def get_fighter(self, rank: FighterRank) -> Fighter:
+        f = self.fighters[rank]
+        if not f:
+            self.fighters[rank] = Fighter(rank)
+        
+        return f
+    
+class Army:
+    army = []
+    
+    def spawn_fighter(self, rank: FighterRank):
+        self.army.append(Fighter(rank))
+        
+    def draw_army(self):
+        for fighter in self.army:
+            if fighter.rank == FighterRank.major:
+                print("M ", end="")
+            elif fighter.rank == FighterRank.sergeant:
+                print("S ", end="")
+            else:
+                print("P ", end="")
+                
+                
+if __name__ == "__main__":
+    army_size = 1000
+    army = Army()
+    
+    for i in range(army_size):
+        r = random.randrange(3)
+        army.spawn_fighter(r)
+    
+    army.draw_army()
